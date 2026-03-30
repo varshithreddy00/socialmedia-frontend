ffa4e6155e0bdb5d6dccb303764dd9ba7ca78c8c
varshithreddy00 <23eg112c37@anurag.edu.in>
2026-03-30 16:20:55 +0530

    Update api.js
    added backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://socialmedia-backend-wsdj.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
